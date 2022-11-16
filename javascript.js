c6a87a22dba02c67fc48c9cb64e769aa089ea200
Mateus Henrique <mateusteccruz@gmail.com>
2022-11-15 23:09:50 -0300

    Primeira versão push

---
+++ javascript.js
@@ -0,0 +1,116 @@
+function mudar_claro_pag_inicial(){
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
+
+    document.getElementById("header_borda_id").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("section_container_img").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_container_2").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_main").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_separacao2").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_footer").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+
+
+
+
+
+
+
+}
+
+function mudar_escuro_pag_inicial(){
+
+  
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("header_borda_id").style.background = "linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("section_container_img").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_container_2").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_main").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_separacao2").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_footer").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+
+
+
+
+
+
+
+
+ 
+
+
+
+}
+
+
+// document.getElementById("id_container_imagem").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+// document.getElementById("id_container_segundo").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+// document.getElementById("sect-conhecimentos").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+// document.getElementById("container_linguagens_id").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
+// document.getElementById("id_container_rodape").style.background = 
+
+// document.getElementById("id_item1").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+// document.getElementById("id_item2").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+// document.getElementById("id_item3").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+// document.getElementById("id_item4").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+// document.getElementById("id_item5").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+// document.getElementById("id_item6").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
+
+
+// document.getElementById("conteudo_id").style.color = "black";
+// document.getElementById("sect-sobre-mim").style.color = "black";
+// document.getElementById("sect-conhecimentos").style.color = "black";
+// document.getElementById("titulo_id_rodape").style.color = "black";
+
+// document.getElementById("titulo_id_rodape").style.borderColor = "#f57c7ce0";
+// document.getElementById("sect-apresentacao").style.borderColor = "#f57c7ce0";
+// document.getElementById("id_container_segundo").style.borderColor = "#f57c7ce0";
+// document.getElementById("sect-conhecimentos").style.borderColor = "#f57c7ce0";
+// document.getElementById("id_container_rodape").style.borderColor = "#f57c7ce0";
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("section_container_img").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("sect-apresentacao").style.borderColor = "black";
+// document.getElementById("id_container_segundo").style.borderColor = "black";
+// document.getElementById("sect-conhecimentos").style.borderColor = "black";
+// document.getElementById("id_container_rodape").style.borderColor = "black";
+
+
+// document.getElementById("id_item1").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+// document.getElementById("id_item2").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+// document.getElementById("id_item3").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+// document.getElementById("id_item4").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+// document.getElementById("id_item5").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+// document.getElementById("id_item6").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
+
+// document.getElementById("container_linguagens_id").style.background = "linear-gradient(to right, #373d42, #262a2d)";
+// document.getElementById("conteudo_id").style.background = "linear-gradient(to right,#373d42 , #262a2d)";
+// document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#373d42, #262a2d)";
+// document.getElementById("id_container_imagem").style.background = "linear-gradient(to right,#373d42, #262a2d)";
+// document.getElementById("id_container_segundo").style.background = "linear-gradient(to right,#373d42, #262a2d)";
+// document.getElementById("sect-conhecimentos").style.background = "linear-gradient(to right,#373d42, #262a2d)";
+// document.getElementById("id_container_rodape").style.background = "linear-gradient(to right, #373d42, #262a2d)";
+
+
+
+// document.getElementById("conteudo_id").style.color = "white";
+// document.getElementById("sect-sobre-mim").style.color = "white";
+// document.getElementById("sect-conhecimentos").style.color = "white";
+// document.getElementById("titulo_id_rodape").style.color = "white";
