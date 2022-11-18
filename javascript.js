05cbfd742886c79069099dedf9a83c85d1931c5a
Mateus Henrique <mateusteccruz@gmail.com>
2022-11-17 23:16:51 -0300

    coloquei cor certa modo claro da pagina index, digitei os textos da empresa e coloquei margin no container produtos pagina produtos

--- javascript.js
+++ javascript.js
@@ -1,17 +1,21 @@
 function mudar_claro_pag_inicial(){
-    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
+ 
 
-    document.getElementById("header_borda_id").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("section_container_img").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_container_2").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_main").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_separacao2").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_footer").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,rgb(126, 252, 0), #2dc653)";
+    document.getElementById("header_borda_id").style.background = "#480ca8"
+    document.getElementById("section_container_img").style.background ="#d9ffe8";
+    document.getElementById("id_container_2").style.background ="#d9ffe8";
+    document.getElementById("id_main").style.background ="#d9ffe8";
+    document.getElementById("id_separacao2").style.background ="#d9ffe8";
+    document.getElementById("id_footer").style.background ="rgb(126, 252, 0)";
+    document.getElementById("id_veja_empresa").style.background ="lawngreen";
+    document.getElementById("id_veja_empresa").style.borderColor ="#500cbd";
+    document.getElementById("id_container_produtos").style.background ="lawngreen";
+    document.getElementById("id_container_produtos").style.borderColor ="#500cbd";
+    document.getElementById("id_separacao1").style.background ="purple";
+    document.getElementById("id_img_setup").style.borderColor ="500cbd ";
 
-
-
-
-
+    
 
 
 }
@@ -26,18 +30,45 @@ function mudar_escuro_pag_inicial(){
     document.getElementById("id_main").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
     document.getElementById("id_separacao2").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
     document.getElementById("id_footer").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_veja_empresa").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_veja_empresa").style.borderColor ="#3c3c42";
+    document.getElementById("id_container_produtos").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_container_produtos").style.borderColor ="#3c3c42";
+    document.getElementById("id_separacao1").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_img_setup").style.borderColor ="#3c3c42";
+
+}
 
 
 
 
 
+function mudar_claro_pag_produtos(){
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_headerBorda").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_main").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_secao1").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_container_inicial_setup").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_separacao").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_container_produtos_venda").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_footer_produto").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
+    document.getElementById("id_container_produtos_venda").style.borderColor ="#3c3c42";
+  
+    
+    
 
+}
 
-
- 
-
-
-
+function mudar_escuro_pag_produtos(){
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_headerBorda").style.background = "linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_main").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_secao1").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_container_inicial_setup").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_separacao").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_container_produtos_venda").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_footer_produto").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_container_produtos_venda").style.borderColor ="#3c3c42";
 }
 
 
