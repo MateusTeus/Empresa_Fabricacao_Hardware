db935b82cafd8f48b65807c8c8be411c1cfbd75e
Mateus Henrique <mateusteccruz@gmail.com>
2022-11-19 15:41:06 -0300

    Terminei o DOM do javascript nas duas paginas (modo claro e escuro), troquei as 2 ultimas imagens do container produto na pagina index, e mudei cores do modo escuro

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ function mudar_claro_pag_inicial(){
     document.getElementById("id_container_produtos").style.background ="lawngreen";
     document.getElementById("id_container_produtos").style.borderColor ="#500cbd";
     document.getElementById("id_separacao1").style.background ="purple";
-    document.getElementById("id_img_setup").style.borderColor ="500cbd ";
+    document.getElementById("id_img_setup").style.borderColor ="#500cbd ";
 
     
 
@@ -23,18 +23,19 @@ function mudar_claro_pag_inicial(){
 function mudar_escuro_pag_inicial(){
 
   
-    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("header_borda_id").style.background = "linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("section_container_img").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_container_2").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_main").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_separacao2").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_footer").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_veja_empresa").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#7b8283, #7b8283)";
+    document.getElementById("header_borda_id").style.background = "linear-gradient(to right,#878787, #878787)";
+    
+    document.getElementById("section_container_img").style.background ="linear-gradient(to right,#7b8283, #7b8283)";
+    document.getElementById("id_container_2").style.background ="linear-gradient(to right,#7b8283, #7b8283)";
+    document.getElementById("id_main").style.background ="linear-gradient(to right,#7b8283, #7b8283)";
+    document.getElementById("id_separacao2").style.background ="linear-gradient(to right,#3c3c42, #3c3c42)";
+    document.getElementById("id_footer").style.background ="linear-gradient(to right,#2d2d2d, #2d2d2d)";
+    document.getElementById("id_veja_empresa").style.background ="linear-gradient(to right,#3c3c42, #3c3c42)";
     document.getElementById("id_veja_empresa").style.borderColor ="#3c3c42";
-    document.getElementById("id_container_produtos").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_container_produtos").style.borderColor ="#3c3c42";
-    document.getElementById("id_separacao1").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("id_container_produtos").style.background ="linear-gradient(to right,#3c3c42, #3c3c42)";
+    document.getElementById("id_container_produtos").style.borderColor ="black";
+    document.getElementById("id_separacao1").style.background ="linear-gradient(to right,#3c3c42, #3c3c42)";
     document.getElementById("id_img_setup").style.borderColor ="#3c3c42";
 
 }
@@ -44,15 +45,15 @@ function mudar_escuro_pag_inicial(){
 
 
 function mudar_claro_pag_produtos(){
-    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_headerBorda").style.background = "linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_main").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_secao1").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_container_inicial_setup").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_separacao").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_container_produtos_venda").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_footer_produto").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-    document.getElementById("id_container_produtos_venda").style.borderColor ="#3c3c42";
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,rgb(126, 252, 0), #2dc653)";
+    document.getElementById("id_headerBorda").style.background = "linear-gradient(to right,#500cbd, #500cbd)";
+    document.getElementById("id_main").style.background ="#d9ffe8";
+    document.getElementById("id_secao1").style.background ="#d9ffe8";
+    document.getElementById("id_container_inicial_setup").style.background ="rgb(126, 252, 0)";
+    document.getElementById("id_separacao").style.background ="purple";
+    document.getElementById("id_container_produtos_venda").style.background ="rgb(126, 252, 0)";
+    document.getElementById("id_footer_produto").style.background ="rgb(126, 252, 0)";
+    document.getElementById("id_container_produtos_venda").style.borderColor ="#480ca8";
   
     
     
@@ -60,88 +61,15 @@ function mudar_claro_pag_produtos(){
 }
 
 function mudar_escuro_pag_produtos(){
-    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_headerBorda").style.background = "linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_main").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_secao1").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_container_inicial_setup").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_separacao").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_container_produtos_venda").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
-    document.getElementById("id_footer_produto").style.background ="linear-gradient(to right,#3c3c42, #7b8283)";
+    document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#7b8283, #7b8283)";
+    document.getElementById("id_headerBorda").style.background = "linear-gradient(to right,#878787, #878787)";
+    document.getElementById("id_main").style.background ="linear-gradient(to right,#7b8283, #7b8283)";
+    document.getElementById("id_secao1").style.background ="linear-gradient(to right,#7b8283, #7b8283)";
+    document.getElementById("id_container_inicial_setup").style.background ="linear-gradient(to right,#3c3c42, #3c3c42)";
+    document.getElementById("id_separacao").style.background ="linear-gradient(to right,#3c3c42, #3c3c42)";
+    document.getElementById("id_container_produtos_venda").style.background ="linear-gradient(to right,#3c3c42, #3c3c42)";
+    document.getElementById("id_footer_produto").style.background ="linear-gradient(to right,#2d2d2d, #2d2d2d)";
     document.getElementById("id_container_produtos_venda").style.borderColor ="#3c3c42";
 }
 
 
-// document.getElementById("id_container_imagem").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
-// document.getElementById("id_container_segundo").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
-// document.getElementById("sect-conhecimentos").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
-// document.getElementById("container_linguagens_id").style.background = "linear-gradient(to right,#FF9CBB, #F3CE5F)";
-// document.getElementById("id_container_rodape").style.background = 
-
-// document.getElementById("id_item1").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
-// document.getElementById("id_item2").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
-// document.getElementById("id_item3").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
-// document.getElementById("id_item4").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
-// document.getElementById("id_item5").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
-// document.getElementById("id_item6").style.background = "linear-gradient(to bottom, #FC79A2, #F7C83E)";
-
-
-// document.getElementById("conteudo_id").style.color = "black";
-// document.getElementById("sect-sobre-mim").style.color = "black";
-// document.getElementById("sect-conhecimentos").style.color = "black";
-// document.getElementById("titulo_id_rodape").style.color = "black";
-
-// document.getElementById("titulo_id_rodape").style.borderColor = "#f57c7ce0";
-// document.getElementById("sect-apresentacao").style.borderColor = "#f57c7ce0";
-// document.getElementById("id_container_segundo").style.borderColor = "#f57c7ce0";
-// document.getElementById("sect-conhecimentos").style.borderColor = "#f57c7ce0";
-// document.getElementById("id_container_rodape").style.borderColor = "#f57c7ce0";
-
-
-
-
-
-
-
-
-
-
-// document.getElementById("section_container_img").style.background ="linear-gradient(to right,#ECD6C4, #F5C59E)";
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementById("sect-apresentacao").style.borderColor = "black";
-// document.getElementById("id_container_segundo").style.borderColor = "black";
-// document.getElementById("sect-conhecimentos").style.borderColor = "black";
-// document.getElementById("id_container_rodape").style.borderColor = "black";
-
-
-// document.getElementById("id_item1").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
-// document.getElementById("id_item2").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
-// document.getElementById("id_item3").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
-// document.getElementById("id_item4").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
-// document.getElementById("id_item5").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
-// document.getElementById("id_item6").style.background = "linear-gradient(to bottom, #804E5E, #876D1E)";
-
-// document.getElementById("container_linguagens_id").style.background = "linear-gradient(to right, #373d42, #262a2d)";
-// document.getElementById("conteudo_id").style.background = "linear-gradient(to right,#373d42 , #262a2d)";
-// document.getElementById("cabecalho_id").style.background = "linear-gradient(to right,#373d42, #262a2d)";
-// document.getElementById("id_container_imagem").style.background = "linear-gradient(to right,#373d42, #262a2d)";
-// document.getElementById("id_container_segundo").style.background = "linear-gradient(to right,#373d42, #262a2d)";
-// document.getElementById("sect-conhecimentos").style.background = "linear-gradient(to right,#373d42, #262a2d)";
-// document.getElementById("id_container_rodape").style.background = "linear-gradient(to right, #373d42, #262a2d)";
-
-
-
-// document.getElementById("conteudo_id").style.color = "white";
-// document.getElementById("sect-sobre-mim").style.color = "white";
-// document.getElementById("sect-conhecimentos").style.color = "white";
-// document.getElementById("titulo_id_rodape").style.color = "white";
